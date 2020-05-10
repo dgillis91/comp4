6b00d2275bbe1c028bc3da9518676849703d12b8
David Gillis <dgillis91@gmail.com>
2020-05-09 22:16:03 -0500

    support for some binary ops

--- generator.py
+++ generator.py
@@ -5,25 +5,39 @@ class NameGenerator:
         self._label_counter = -1
         self._variable_prefix = variable_prefix
         self._label_prefix = label_prefix
+        self.variables = list()
 
     def make_label(self):
-        self._label_prefix += 1
+        self._label_counter += 1
         return '{}{}'.format(self._label_prefix, self._label_counter)
 
     def make_variable(self):
-        self._variable_prefix += 1
-        return '{}{}'.format(self._variable_prefix, self._variable_counter)
+        self._variable_counter += 1
+        var = '{}{}'.format(self._variable_prefix, self._variable_counter)
+        self.add_variable_to_list(var)
+        return var
+
+    def add_variable_to_list(self, name, initial_value=0):
+        self.variables.append((name, initial_value))
 
 
 NO_ACTION_SET = set([
-    'program', 'block', 'stats', 'mstat', 'stat'
+    'program', 'block', 'stats', 'mstat', 'stat',
+    # Not actually no_action
+    'a', 'm'
 ])
 
+OPERATION_MAP = {
+    '/': 'DIV',
+    '*': 'MUL'
+}
 
-variables = list()
+
+name_generator = NameGenerator()
+
 
 def code_generator_rec(target_path, node):
-    global variables
+    global name_generator
     if node is None:
         return
     elif node.label in NO_ACTION_SET:
@@ -31,20 +45,59 @@ def code_generator_rec(target_path, node):
             code_generator_rec(target_path, child)
         return
     elif node.label == 'variables' and node.tokens:
-        variables.append((node.tokens[0].payload, node.tokens[2].payload))
+        name_generator.add_variable_to_list(node.tokens[0].payload, node.tokens[2].payload)
         for child in node.children:
             code_generator_rec(target_path, child)
         return
     elif node.label == 'intk':
         print('READ {}'.format(node.tokens[0].payload))
         return
+    elif node.label == 'expr':
+        # Expands down to N
+        if len(node.children) == 1:
+            code_generator_rec(target_path, node.children[0])
+        # Expands to subtraction
+        else:
+            code_generator_rec(target_path, node.children[1])
+            temp_var_name = name_generator.make_variable()
+            print('STORE {}'.format(temp_var_name))
+            code_generator_rec(target_path, node.children[0])
+            print('SUB {}'.format(temp_var_name))
+        return 
+    elif node.label == 'out':
+        code_generator_rec(target_path, node.children[0])
+        temp_var_name = name_generator.make_variable()
+        print('STORE {}'.format(temp_var_name))
+        print('WRITE {}'.format(temp_var_name))
+        return
+    # TODO: Add support for parens.
+    elif node.label == 'r':
+        if node.tokens[0].group in ['digit', 'id']:
+            print('LOAD {}'.format(node.tokens[0].payload))
+        return 
+    elif node.label == 'assign':
+        code_generator_rec(target_path, node.children[0])
+        print('STORE {}'.format(node.tokens[0].payload))
+        return
+    elif node.label == 'n':
+        # Expands to a
+        if len(node.tokens) == 0:
+            code_generator_rec(target_path, node.children[0])
+        else:
+            operation = OPERATION_MAP[node.tokens[0].payload]
+            code_generator_rec(target_path, node.children[1])
+            temp_var_name = name_generator.make_variable()
+            print('STORE {}'.format(temp_var_name))
+            code_generator_rec(target_path, node.children[0])
+            print('{} {}'.format(operation, temp_var_name))
+        return 
 
 
 
 def code_generator(target_path, tree):
-    global variables
+    global name_generator
     code_generator_rec(target_path, tree)
     print('STOP')
-    for variable_name, variable_init in variables:
+    for variable_name, variable_init in name_generator.variables:
         print('{} {}'.format(variable_name, variable_init))
     return
